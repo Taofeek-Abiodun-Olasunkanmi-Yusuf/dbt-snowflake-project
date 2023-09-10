a028c3a31d380feff678ed10cf603d52520de54a
Taofeek Abiodun Olasunkanmi Yusuf <taofeekaoyusuf@gmail.com>
2023-09-10 22:53:44 +0000

    newly modified file

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -2,10 +2,10 @@ with orders as (
     select
         orders.id as order_id,
         orders.user_id as customer_id,
-        payments.amount as amount
+        raw.stripe.payment.amount as amount
     from raw.jaffle_shop.orders
     inner join raw.stripe.payment 
-    on orders.order_id = payments.order_id
+    on orders.order_id = raw.stripe.payment.order_id
 )
 
 select * from orders
