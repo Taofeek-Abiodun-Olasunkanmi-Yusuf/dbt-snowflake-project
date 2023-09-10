3fa6582701757a738156348528d24e7797a58bf5
Taofeek Abiodun Olasunkanmi Yusuf <taofeekaoyusuf@gmail.com>
2023-09-10 22:57:30 +0000

    file modify

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,7 +1,7 @@
 with orders as (
     select
-        orders.id as order_id,
-        orders.user_id as customer_id,
+        raw.jaffle_shop.orders.id as order_id,
+        raw.jaffle_shop.orders.user_id as customer_id,
         raw.stripe.payment.amount as amount
     from raw.jaffle_shop.orders
     inner join raw.stripe.payment 
